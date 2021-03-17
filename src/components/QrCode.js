ccdf5cc2e472a335258259813d2bc309a9677e6e
Anthony <a.leclercq55@live.fr>
2021-03-17 13:33:20 +0100

    Ajout du Scanner QR CODE

---
+++ src/components/QrCode.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, Image } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import Constants from 'expo-constants';
+import { Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const qrSize = width * 0.7;
+
+export default function QrCode() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+    if (result != "") {
+        alert(result);
+    }
+  }, [result]);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setResult(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View
+    style={{
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+    }}>
+    <BarCodeScanner
+      onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      style={[StyleSheet.absoluteFillObject, styles.container]}>
+      <Text style={styles.description}>Scanner votre QR Code</Text>
+      <Image
+        style={styles.qr}
+        source={require('../../assets/img/Qr.png')}
+      />
+      {scanned && (
+      <Button title={'Tap to Scan Again'} onPress={() => { setScanned(false); setResult("") } } />
+    )}
+      <Text onPress={() => alert("Navigate back from here")} style={styles.cancel}>
+        Cancel
+      </Text>
+    </BarCodeScanner>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: Constants.statusBarHeight,
+      backgroundColor: '#ecf0f1',
+      padding: 8,
+    },
+    qr: {
+      marginTop: '20%',
+      marginBottom: '20%',
+      width: qrSize,
+      height: qrSize,
+    },
+    description: {
+      fontSize: width * 0.09,
+      marginTop: '10%',
+      textAlign: 'center',
+      width: '70%',
+      color: 'white',
+    },
+    cancel: {
+      fontSize: width * 0.05,
+      textAlign: 'center',
+      width: '70%',
+      color: 'white',
+    },
+  });
