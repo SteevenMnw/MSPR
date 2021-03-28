891cca0e75b80f6bff5b615e7dd48cb4aff38114
Your NameSteeven <steeven.m59@hotmail.fr>
2021-03-28 19:45:44 +0200

    fix SignIn

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, Text} from "react-native";
 
 import { Input, Button } from 'react-native-elements';
 import Ionicons from "react-native-vector-icons/Ionicons";
