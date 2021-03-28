ede10cb84f9a6ea5d9d1875c17ea4dcf9151f42a
Anthony <a.leclercq55@live.fr>
2021-03-28 13:23:41 +0200

    Ajout de la fonctionnalité Scan QrCode

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import QrCode from './src/components/QrCode'
+import QrCode from "./src/components/QrCode";
 
 export default function App() {
-  return (
-    <QrCode />
-  );
+  return <QrCode />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/API/API_Access.js
+++ src/API/API_Access.js
@@ -1,45 +1,43 @@
 import axios from "axios";
 
-const BASE_URL = "https://springmspr.herokuapp.com/"
+const BASE_URL = "https://springmspr.herokuapp.com/";
 
 // Récupère tous les coupons
 export function getAllCoupons() {
-    const url = BASE_URL + "/coupons/all";
-    return axios.get(url)
-        .then(response => response.data)
+  const url = BASE_URL + "/coupons/all";
+  return axios.get(url).then((response) => response.data);
 }
 
 // Récupère un coupon grâce à l'id
 export function getCouponById(id) {
-    const url = BASE_URL + "coupons/" + id
-    return axios.get(url)
-        .then(response => response.data)
+  const url = BASE_URL + "coupons/" + id;
+  return axios.get(url).then((response) => response.data);
 }
 
 // Récupère tous les utilisateurs
 export function getAllUsers() {
-    const url = BASE_URL + "/users/all";
-    return axios.get(url)
-        .then(response => response.data)
+  const url = BASE_URL + "/users/all";
+  return axios.get(url).then((response) => response.data);
 }
 
 // Récupère l'utilisateur grâce à l'id
 export function getUserById(id) {
-    const url = BASE_URL + "/users/" + id;
-    return axios.get(url)
-        .then(response => response.data)
+  const url = BASE_URL + "/users/" + id;
+  return axios.get(url).then((response) => response.data);
 }
 
 // Récupère l'utilisateur grâce à son email et son password
 export function getUserByEmailAndPassword(email, password) {
-    const url = BASE_URL + "/users/identification";
-    return axion.get(url, {params: {email: email, password: password} })
-        .then(response => response.data)
+  const url = BASE_URL + "/users/identification";
+  return axios
+    .get(url, { params: { email: email, password: password } })
+    .then((response) => response.data);
 }
 
 // Ajout d'un coupon à l'utilisateur en utilisant l'id du coupon, l'id de l'utilisateur
 export function addCouponForUser(idUser, idCoupon) {
-    const url = BASE_URL + "/users/addCoupon";
-    return axios.put(url, {idUser: idUser, idCoupon: idCoupon})
-        .then(response => response.data)
+  const url = BASE_URL + "/users/addCoupon";
+  return axios
+    .put(url, null, { params: { idUser, idCoupon } })
+    .then((response) => response.data);
 }
