c7e32fae6d51f0c5cb24560220ab4700818c217c
Your NameSteeven <steeven.m59@hotmail.fr>
2021-03-29 11:06:30 +0200

    fix

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,10 +3,10 @@ import { View, StyleSheet, Text } from "react-native";
 
 import { Input, Button } from "react-native-elements";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { addUser } from "../API/API_Access";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import * as Crypto from "expo-crypto";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { addUser } from '../API/API_Access';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import * as Crypto from "expo-crypto"
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -103,14 +103,26 @@ class SignUp extends React.Component {
     }
   };
 
-  render() {
-    return (
-      <View style={styles.main_container}>
-        <View style={styles.Input}>
-          <Input
-            placeholder="Name"
-            leftIcon={
-              <Ionicons name="person-outline" size={24} color="black" />
+    signUp = () => {
+        try {
+            name = this.state.name
+            surname = this.state.surname
+            email = this.state.email
+            passwd = this.state.passwd
+            checkPasswd = this.state.checkPasswd
+            if(passwd == checkPasswd && passwd != "" && checkPasswd != "" && name && surname && email){
+                if(email.includes("@") && email.includes(".com") || email.includes("@") && email.includes(".fr")){
+                    const value = Crypto.digestStringAsync( Crypto.CryptoDigestAlgorithm.SHA256, passwd );
+                    value.then((newPasswd) => {
+                        console.log(newPasswd)
+                        this.setUserSession(email, newPasswd, name, surname)
+                        addUser(email, newPasswd, name, surname)
+                        // navigation.navigate("Home") a faire ----------------------------------
+                    })
+                }
+                else{
+                    this.updateError("Please enter a correct email")
+                }
             }
             onChangeText={this.updateName}
             value={this.state.name}
@@ -135,40 +147,103 @@ class SignUp extends React.Component {
             leftIcon={
               <Ionicons name="lock-closed-outline" size={24} color="black" />
             }
-            onChangeText={this.updatePasswd}
-            value={this.state.passwd}
-          />
-          <Input
-            secureTextEntry={true}
-            placeholder="Confirm Password"
-            leftIcon={
-              <Ionicons name="lock-closed-outline" size={24} color="black" />
-            }
-            onChangeText={this.updatecheckPasswd}
-            value={this.state.checkPasswd}
-          />
-        </View>
-        <View>
-          <Text style={styles.Error}>{this.state.error}</Text>
-          <Button
-            buttonStyle={{ width: 150, alignSelf: "center" }}
-            title="Sign up"
-            onPress={this.signUp}
-          />
-        </View>
-        <View style={{ bottom: -125, alignSelf: "center" }}>
-          <Text>Already have an account ?</Text>
-          <TouchableOpacity
-            style={{
-              alignSelf: "center",
-            }} /*onPress={() => navigation.navigate("SignUp")} a faire ---------------------------------*/
-          >
-            <Text style={{ color: "#0099ff" }}>Sign In.</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
+        }
+        catch(e) {
+            console.log(e);
+        }
+    }
+
+    render() {
+
+        return (
+            <View style={styles.main_container}>
+                <View style={styles.Input}>
+                    <Input
+                        placeholder='Name'
+                        leftIcon={
+                            <Ionicons
+                                name='person-outline'
+                                size={24}
+                                color='black'
+                            />
+                        }
+                        onChangeText={ this.updateName }
+                        value={ this.state.name }
+                    />
+                    <Input
+                        placeholder='Surname'
+                        leftIcon={
+                            <Ionicons
+                                name='people-outline'
+                                size={24}
+                                color='black'
+                            />
+                        }
+                        onChangeText={ this.updateSurname }
+                        value={ this.state.surname }
+                    />
+                    <Input
+                        placeholder='Email'
+                        leftIcon={
+                            <Ionicons
+                                name='mail-outline'
+                                size={24}
+                                color='black'
+                            />
+                        }
+                        onChangeText={ this.updateEmail }
+                        value={ this.state.email }
+                    />
+                    <Input
+                        secureTextEntry={true}
+                        placeholder='Password'
+                        leftIcon={
+                            <Ionicons
+                                name='lock-closed-outline'
+                                size={24}
+                                color='black'
+                            />
+                        }
+                        onChangeText={ this.updatePasswd }
+                        value={ this.state.passwd }
+                    />
+                    <Input
+                        secureTextEntry={true}
+                        placeholder='Confirm Password'
+                        leftIcon={
+                            <Ionicons
+                                name='lock-closed-outline'
+                                size={24}
+                                color='black'
+                            />
+                        }
+                        onChangeText={ this.updatecheckPasswd }
+                        value={ this.state.checkPasswd }
+                    />
+                </View>
+                <View>
+                    <Text style={styles.Error}>
+                        { this.state.error }
+                    </Text>
+                    <Button
+                        buttonStyle={{width: 150, alignSelf:"center"}}
+                        title="Sign up"
+                        onPress={ this.signUp }
+                    />
+                </View>
+                <View style={{ bottom: -125, alignSelf:"center" }}>
+                    <Text>
+                        Already have an account ? 
+                    </Text>
+                    <TouchableOpacity style={{ alignSelf:"center" }} /*onPress={() => navigation.navigate("SignUp")} a faire ---------------------------------*/ >
+                        <Text style={{ color: "#0099ff" }}>
+                            Sign In.
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
