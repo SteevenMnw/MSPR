c339c5e490289b40e68a6c25a61fa91fbde6b435
Anthony <a.leclercq55@live.fr>
2021-03-29 12:06:27 +0200

    fix

--- src/API/API_Access.js
+++ src/API/API_Access.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://springmspr.herokuapp.com";
 // Récupère tous les coupons
 // https://springmspr.herokuapp.com/coupons/all
 export function getAllCoupons() {
-  const url = BASE_URL + "coupons/all";
+  const url = BASE_URL + "/coupons/all";
   return axios.get(url).then((response) => response.data);
 }
 
@@ -19,21 +19,21 @@ export function getCouponById(id) {
 // Récupère tous les utilisateurs
 // https://springmspr.herokuapp.com/users/all
 export function getAllUsers() {
-  const url = BASE_URL + "users/all";
+  const url = BASE_URL + "/users/all";
   return axios.get(url).then((response) => response.data);
 }
 
 // Récupère l'utilisateur grâce à l'id
 // https://springmspr.herokuapp.com/users/1
 export function getUserById(id) {
-  const url = BASE_URL + "users/" + id;
+  const url = BASE_URL + "/users/" + id;
   return axios.get(url).then((response) => response.data);
 }
 
 // Récupère l'utilisateur grâce à son email et son password
 // https://springmspr.herokuapp.com/users/identification
 export function getUserByEmailAndPassword(email, password) {
-  const url = BASE_URL + "users/identification";
+  const url = BASE_URL + "/users/identification";
   return axios
     .get(url, { params: { email: email, password: password } })
     .then((response) => response.data);
@@ -49,35 +49,8 @@ export function addCouponForUser(idUser, idCoupon) {
 }
 
 // Ajout d'un utilisateur
-// https://springmspr.herokuapp.com//users/add
-export function addUser(email, password, name, surname) {
-  const url = BASE_URL + "users/add";
-  return axios(url, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      password,
-      name,
-      surname,
-    }),
-  })
-    .then((response) => console.log(response))
-    .catch((e) => console.error(e));
 // https://springmspr.herokuapp.com/users/add
-export function addUser(mail, password, name, surname) {
+export function addUser(email, password, name, surname) {
   const url = BASE_URL + "/users/add";
-  return axios
-    .post(url, null, {
-      params: {
-        mail,
-        password,
-        name,
-        surname,
-      },
-    })
-    .catch((error) => console.log(error));
+  return axios.post(url, null, { params: { email, password, name, surname } });
 }
