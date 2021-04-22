2ed53e9e744a262f6a398c18014143f64e3a189a
Julien <julien.chassaing1@gmail.com>
2021-04-22 18:19:32 +0200

    Test homepage

--- __tests__/pages/Home.test.js
+++ __tests__/pages/Home.test.js
@@ -7,13 +7,21 @@ import { expect, it } from "@jest/globals";
 
 jest.useFakeTimers();
 
-jest.mock('react-native-tiny-toast', () => ({
+jest.mock("react-native-tiny-toast", () => ({
   show: jest.fn(),
-  hide: jest.fn()
+  hide: jest.fn(),
 }));
 
-// Snapshop de l'ensemble de la page Home
-it("renders correctly", () => {
-  const tree = renderer.create(<Home />).toJSON();
-  expect(tree).toMatchSnapshot();
+// Unit test (https://docs.expo.io/guides/testing-with-jest/#unit-test)
+describe("<Home />", () => {
+  it("has 1 child", () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
+
+  // Snapshop de l'ensemble de la page Home
+  it("renders correctly", () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
